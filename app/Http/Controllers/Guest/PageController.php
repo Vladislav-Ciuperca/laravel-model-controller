11aafbfbae985b7fa48ad7a2781e92566ca76dbd
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-07-03 19:34:41 +0200

    esercizion ok

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Movies;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+
+   
+
+         $movies = movies::all();
+
+    
+
+        $dati = [
+            "store" => config("data"),
+            "movies" => $movies,
+        ];
+
+        return view('home', $dati);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da "config/data.php": {{ $home }}</h1>
+    <h1>Dati da "config/data.php": {{ $store["home"] }}</h1>
     <p>This is my paragraph content.</p>
+    <button >Cliccami</button>
+
+
+    @foreach ($movies as $singleMovie)
+        <p>{{$singleMovie ["title"]}}</p>
+    @endforeach
+
 @endsection
